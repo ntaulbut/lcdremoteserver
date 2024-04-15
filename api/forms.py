2e1df12f00e64b4b06b0f2f7ccb91fe3542990ea
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-04-15 01:13:41 +0100

    update input settings

--- api/forms.py
+++ api/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class MessageForm(forms.Form):
-    row_one = forms.CharField(max_length=16, blank=True)
-    row_two = forms.CharField(max_length=16, blank=True)
+    row_one = forms.CharField(max_length=16, required=False, strip=False)
+    row_two = forms.CharField(max_length=16, required=False, strip=False)
