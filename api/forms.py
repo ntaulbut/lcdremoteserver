ade6d73ff0bfbba829b28b63b85852e4a0b53296
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-04-15 01:10:38 +0100

    allow blank rows

--- api/forms.py
+++ api/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class MessageForm(forms.Form):
-    row_one = forms.CharField(max_length=16)
-    row_two = forms.CharField(max_length=16)
+    row_one = forms.CharField(max_length=16, blank=True)
+    row_two = forms.CharField(max_length=16, blank=True)
